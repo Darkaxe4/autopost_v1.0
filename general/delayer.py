7fb62323991819866af7791a8c401ef778034e62
KIOrlov <Konstantin.iv.orlov@gmail.com>
2025-02-10 04:36:41 +0300

    Made delayer class

--- general/delayer.py
+++ general/delayer.py
@@ -1,27 +1,29 @@
 import asyncio
 from datetime import datetime, timedelta
+from typing import Callable
 
 # Define a callback function
 def my_callback():
     print(f"Callback executed at {datetime.now().timestamp()}")
 
+class Delayer:
+    def __init__ (self):
+        self.loop = asyncio.get_event_loop()
+
+    async def delayFunction(self, time:datetime, func:Callable):
+        now = self.loop.time()
+        timedelay = time - datetime.now()
+        timestamp = now + timedelay.total_seconds()
+        self.loop.call_at(timestamp, func)
+
 async def main():
-    # Get the event loop
-    loop = asyncio.get_event_loop()
 
-    # Get the current time
-    now = loop.time()
-    loop.time()
-    print(now)
-
-    # Schedule the callback to run 5 seconds from now
-    timestamp = now + 5
-    loop.call_at(timestamp, my_callback)
-
-    print(f"Scheduled callback to run at {timestamp}")
+    bubylda = Delayer()
+    await bubylda.delayFunction(datetime.now()+timedelta(seconds=5), my_callback)
 
     # Keep the event loop running for a while to allow the callback to execute
-    await asyncio.sleep(6)  # Wait longer than the scheduled time
+    while True:
+        await asyncio.sleep(1)  # Wait longer than the scheduled time
 
 # Run the asyncio event loop
 asyncio.run(main())
